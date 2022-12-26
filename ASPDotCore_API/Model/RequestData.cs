ddbfa7e5ee81e6de10ac261922860ea0a2f91336
Piyush Adhikari <padhikari@microsoft.com>
2022-12-26 22:48:11 +0530

    initial commit

---
+++ ASPDotCore_API/Model/RequestData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPDotCore_API.Model
+{
+    public class RequestData
+    {
+        public string name { get; set; }
+    }
+}
